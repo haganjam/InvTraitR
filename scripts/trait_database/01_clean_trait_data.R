08862e99b3d3be75273175f01b0da198e828c445
James Hagan <james_hagan@outlook.com>
2022-03-14 18:19:50 +0100

    add a script to clean the trait data

---
+++ scripts/trait_database/01_clean_trait_data.R
@@ -0,0 +1,115 @@
+
+# Clean trait data
+
+# load relevant libraries
+library(here)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+
+## Axell 2021:
+
+# load the source list
+axell.s <- read_delim(file = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_4_BEF_rockpools_Australia/data/trait_and_allometry_data/allometry_database/papers_to_get_test_data_from/Axell_2021/Database_references.txt", 
+                      delim = "\t")
+
+axell.s[axell.s$`Source code` == 37, ]$Reference
+
+# load the data
+axell.d <- read_delim(file = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_4_BEF_rockpools_Australia/data/trait_and_allometry_data/allometry_database/papers_to_get_test_data_from/Axell_2021/Database_invertebrates.txt", 
+                      delim = "\t")
+head(axell.d)
+
+# check parsing problems
+problems(axell.d)
+
+# fix the data loading problems
+axell.d[1046, ]$`NFE (value)` <- 21.9
+axell.d[1095, ]$`NFE (value)` <- 0.9
+
+# check the added logical column
+names(axell.d)
+
+# the logical column is only FALSE values
+any(!is.na(axell.d$...41))
+
+# remove the ...41 variable which was a parsing failure
+axell.d <- axell.d[, -41]
+
+# subset the relevant rows of data
+axell.lw <- 
+  axell.d %>%
+  filter( (!is.na(Genus)) & (!is.na(`TL (value)`)) ) %>%
+  select(Genus, Species, LifeStage,
+         `TL (value)`, `TL (reference)`,
+         `DW (value)`, `DW (reference)`)
+
+## TL variable
+# split the TL (value) variable
+x <- strsplit(axell.lw$`TL (value)`, split = "-") 
+
+# add a min and max column
+axell.lw$TL_min <- 
+  unlist( 
+    lapply(x, function(y) { 
+      z <- gsub(pattern = "\\+/", replacement = "", y[1])
+      gsub( pattern = "\\ .*", replacement = "", z) } )
+  )
+
+axell.lw$TL_max <- 
+  unlist( 
+    lapply(x, function(y) gsub(pattern = "]", replacement = "", y[2]) )
+    )
+
+# add a median column
+axell.lw$TL_middle <- ifelse(is.na(axell.lw$TL_max), axell.lw$TL_min, NA)
+
+# remove the duplicate minima 
+axell.lw$TL_min <- ifelse(!is.na(axell.lw$TL_middle), NA, axell.lw$TL_min)
+
+## DW variable 
+# split the DW variable
+x <- strsplit(axell.lw$`DW (value)`, split = "-") 
+
+# create a middle variable
+axell.lw$DW_middle <- 
+  unlist( 
+  lapply(x, function(y) ifelse(length(y) == 1, y[1], NA )) 
+  ) 
+
+# create a min variable
+axell.lw$DW_min <- 
+  unlist( 
+    lapply(x, function(y) y[1])
+    )
+
+# create a max variable
+axell.lw$DW_max <- 
+  unlist( 
+    lapply(x, function(y) y[2]) 
+    )
+
+# remove the duplicate minima 
+axell.lw$DW_min <- ifelse(!is.na(axell.lw$DW_middle), NA, axell.lw$DW_min)
+
+## Reorder
+# reorganise the columns
+axell.lw <- 
+  axell.lw %>%
+  select(Genus, Species, LifeStage, TL_reference = `TL (reference)`,
+         TL_middle, TL_min, TL_max, DW_reference = `DW (reference)`,
+         DW_middle, DW_min, DW_max)
+
+
+## Tachet
+
+
+
+
+
+
+
+
+
+
