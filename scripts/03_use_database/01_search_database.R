bead34cad865b6d9f5e0bbead090a99e682b12f2
James Hagan <james_hagan@outlook.com>
2022-07-08 17:35:15 +0200

    try out the search fnuctions

--- scripts/03_use_database/01_search_database.R
+++ scripts/03_use_database/01_search_database.R
@@ -11,6 +11,271 @@ library(igraph)
 library(bdc)
 library(taxadb)
 
+# write functions for:
+
+# 1. cleaning the input data target taxa robustly using the bdc package
+
+# - data.frame scale
+
+# 2. get habitat data for the target taxa
+
+# - data.frame scale
+
+# 3. calculate taxonomic distance habitat for each target taxa
+
+# - taxa by taxa
+
+# 4. merge all this information to make a decision
+
+
+
+
+
+
+
+#'
+#' @title extract_genus()
+#' 
+#' @description If a binomial taxa is supplied, extract the genus name only
+#' 
+#' @details This method works almost exclusively with genera and not species. This is because
+#' once the genus is known, the relationship among the species within that genus is known. It
+#' makes the method much more computationally efficient. This function is used to then extract
+#' the genus from a species name. The genus name is used to place the species in the correct
+#' taxonomic framework 
+#' 
+#' @author James G. Hagan (james_hagan(at)outlook.com)
+#' 
+#' @param binomial - binomial character string separated by a space (e.g. "Loxodonta loxodonta")
+#' 
+#' @return string with the genus name
+#' 
+
+extract_genus <- function(binomial) {
+  z <- unlist( strsplit(x = binomial, split = " ", fixed = TRUE) )
+  z.l <- length(z)
+  if (z.l > 1) {
+    search.name <- z[1]
+  } else {
+    search.name <- binomial
+  }
+  # add a word count attribute
+  attr(search.name, "n") <- z.l
+  return(search.name)
+}
+
+#'
+#' @title Get_tax_dist()
+#' 
+#' @description Get taxonomic distances of target names relative to the taxa databases
+#' 
+#' @details This function takes a data.frame with target_taxa and searches a set of taxonomic
+#' graphs from gbif, itis and col to find taxa that are closely related. The names are first
+#' cleaned and harmonised using the bdc package. Then, prep-prepared graphs that
+#' contain taxonomic information are searched and distances between taxa in our database
+#' and the target taxon are identified.
+#' 
+#' @author James G. Hagan (james_hagan(at)outlook.com)
+#' 
+#' @param data - data.frame with a column containing target taxon names
+#' @param database - taxonomic database to use: gbif, itis, col
+#' @param target_taxon - character string with the column name containing the taxon names
+#' 
+#' @return data.frame with target taxon names, database names and relevant taxonomic distances
+#' 
+
+# write a function to get the taxonomic distances for database entries
+Get_tax_dist <- function(data, database = "gbif", target_taxon) {
+  
+  t.dat <- 
+    data %>%
+    rename(target_taxon = target_taxon)
+  
+  # clean the names for typos etc.
+  cleaned_names <- bdc_clean_names(sci_names = t.dat[["target_taxon"]], save_outputs = FALSE)
+  
+  # write some code to remove the output file
+  unlink("Output", recursive=TRUE)
+  
+  # check if any names were changed
+  if ( !any(cleaned_names$scientificName != cleaned_names$names_clean) ) {
+    message("No names were changed")
+  }
+  
+  # replace the names in tax.dat with these cleaned names
+  t.dat$target_taxon <- cleaned_names$names_clean
+  
+  t.dat <- 
+    t.dat %>%
+    mutate(row_id = 1:n()) %>%
+    dplyr::select(row_id, target_taxon)
+  
+  # create the local database
+  td_create(
+    provider = database,
+    overwrite = FALSE)
+  
+  # harmonise the names to the gbif database
+  harm.tax <- 
+    bdc_query_names_taxadb(sci_name = t.dat$target_taxon,
+                           db = database,
+                           rank_name = "Animalia",
+                           rank = "kingdom"
+    )
+  
+  # write some code to remove the output file
+  unlink("Output", recursive=TRUE)
+  
+  harm.tax <- 
+    harm.tax %>%
+    mutate(db_taxon_higher_rank = ifelse(is.na(order) & is.na(family), NA, 
+                                         ifelse(is.na(order) & !is.na(family), "family", "order") ) ) %>%
+    mutate(db_taxon_higher = ifelse(is.na(order) & is.na(family), NA, 
+                                    ifelse(is.na(order), family, order) ) ) %>%
+    mutate(db_higher_rank_source = database) %>%
+    mutate(row_id = 1:n()) %>%
+    dplyr::select(row_id, original_search, scientificName, acceptedNameUsageID, db_higher_rank_source, db_taxon_higher_rank, db_taxon_higher)
+  
+  # remove the names that we were not able to resolve
+  harm.tax <- 
+    harm.tax %>%
+    filter(!(is.na(scientificName) |is.na(db_taxon_higher_rank) | is.na(db_taxon_higher) ) ) %>%
+    rename(target_taxon = original_search)
+  
+  # join these data to the tax.dat data
+  t.clean <- right_join(t.dat, harm.tax, by = c("row_id", "target_taxon") )
+  
+  # check that the join worked correctly
+  if ( nrow(harm.tax) == nrow(t.clean) ) {
+    message("Join worked correctly")
+  }
+  
+  # remove the row_id column
+  t.clean <- 
+    t.clean %>%
+    dplyr::select(-row_id)
+  
+  # check if any of these names could be found and if not, return a blank data.frame
+  if(nrow(t.clean) == 0) {
+    
+    t.dat$db_source <- database
+    return( t.dat[, c("target_taxon", "db_source") ] )
+    
+  }
+  
+  dist.list <- vector("list", length = nrow(t.clean))
+  for(i in 1:nrow(t.clean)) {
+    
+    # extract the target name
+    targ.higher <- t.clean[i,][["db_taxon_higher"]]
+    clean.targ <- t.clean[i,][["scientificName"]]
+    targ <- t.clean[i,][["target_taxon"]]
+    
+    # check if there are any relevant names to check
+    if (is.na(targ.higher) | is.na(clean.targ) ) {
+      
+      dist.df <- 
+        tibble(target_taxon = targ, 
+               clean_target_name = clean.targ,
+               database_name = NA,
+               tax_distance = NA)
+      
+      dist.list[[i]] <- dist.df
+      
+    } else {
+      
+      # get the correct database
+      if(database == "gbif") {
+        
+        tax.graph <- gbif_htm[which(names(gbif_htm) == targ.higher)][[1]]
+        v.x <- V(tax.graph)
+        
+        tax.td <- 
+          gbif_td %>%
+          filter(db_taxon_higher == targ.higher)
+        
+      } else if (database == "itis") {
+        
+        tax.graph <- itis_htm[which(names(itis_htm) == targ.higher)][[1]]
+        v.x <- V(tax.graph)
+        
+        tax.td <- 
+          itis_td %>%
+          filter(db_taxon_higher == targ.higher)
+        
+      } else if (database == "col") {
+        
+        tax.graph <- col_htm[which(names(col_htm) == targ.higher)][[1]]
+        v.x <- V(tax.graph)
+        
+        tax.td <- 
+          col_td %>%
+          filter(db_taxon_higher == targ.higher)
+        
+      } else {
+        
+        stop("Choose appropriate database: gbif, itis, col")
+        
+      }
+      
+      # get taxonomic distance between target name and database names
+      tax.dist <- 
+        lapply(tax.td$scientificName, function(y) {
+          
+          # extract genus for species-level names
+          tn <- extract_genus(clean.targ)
+          sn <- extract_genus(y)
+          
+          x <- 
+            distances(tax.graph, 
+                      v.x[which(attr(v.x, "names") == tn)],
+                      v.x[which(attr(v.x, "names") == sn)],
+                      mode = c("all"),
+                      algorithm = c("bellman-ford"))
+          
+          if(length(x) == 0) {
+            x <- 0
+          } else {
+            x <- x[[1]]
+          }
+          
+          # extra distance for species level: 0.5
+          sp.l <- sum(ifelse(c(attr(tn, "n"), attr(sn, "n")) > 1, 0.5, 0))
+          
+          dist.df <- 
+            tibble(target_taxon = targ, 
+                   clean_target_name = clean.targ,
+                   database_name = y,
+                   tax_distance = x + sp.l
+            )
+          
+          return(dist.df)
+          
+        } )
+      
+      # bind into a data.frame
+      tax.dist <- bind_rows(tax.dist, .id = "id_rep")
+      
+      # add a data.base identifier
+      tax.dist$db_source <- database
+      
+      tax.dist <- 
+        tax.dist %>%
+        dplyr::select(target_taxon, db_source, clean_target_name, id_rep, database_name, tax_distance)
+      
+      dist.list[[i]] <- tax.dist
+      
+    }
+    
+  }
+  
+  dist.list <- bind_rows(dist.list)
+  
+  return(dist.list)
+  
+}
+
+
 # load the taxon data
 if (!exists("itis_td")) {
   itis_td <- readRDS(file = here("database/itis_taxon_database.rds"))
@@ -52,86 +317,45 @@ if (!exists("equ.dat")) {
 }
 head(equ.dat)
 
+
 # test data.frame
-t.dat <- data.frame(name = "Coloburiscus",
-                    life_stage = "nymph",
-                    length_mm = 10,
-                    lat = c(-25.18),
-                    lon = c(147.69))
-t.dat
+t.dat <- tibble(name = c("Coloburiscus", "Zelandoperla"),
+                life_stage = c("nymph", "adult"),
+                length_mm = c(10, 20),
+                lat = c(-25.18, -25),
+                lon = c(147.69, 145.5)
+)
 
-# choose the taxonomic database: "gbif", "itis", "col"
-database <- "gbif"
 
-# create the local database
-td_create(
-  provider = database,
-  overwrite = FALSE)
-
-# clean the names for typos etc.
-x <- bdc_clean_names(sci_names = t.dat$name, save_outputs = FALSE)
-
-# check if any names were changed
-if ( !any(x$scientificName != x$names_clean) ) {
-  message("No names were changed")
+# loop over all the relevant data.bases then extract the possible names
+Tax_dist_selector <- function(data, target_taxon, max_taxdist = 2.5) {
+  
+  tax.df <- 
+    lapply(c("gbif", "itis", "col"), function(x) {
+    
+    tax <- Get_tax_dist(data = data, 
+                        database = x, 
+                        target_taxon = target_taxon)
+    
+    return(tax)
+    
+  } )
+  
+  # bind the rows together
+  tax.df <- bind_rows(tax.df)
+  
+  # keep the rows where taxonomic distance is low enough
+  tax.df <- 
+    tax.df %>%
+    filter(tax_distance < max_taxdist)
+  
+  return(tax.df)
+  
 }
 
-# replace the names in tax.dat with these cleaned names
-t.dat$db_taxon <- x$names_clean
+x <- Tax_dist_selector(data = t.dat, target_taxon = "name", max_taxdist = 2.5)
 
-t.dat <- 
-  t.dat %>%
-  mutate(row_id = 1:n()) %>%
-  dplyr::select(row_id, db_taxon, life_stage, length_mm, lat, lon)
 
-# harmonise the names to the gbif database
-harm.tax <- 
-  bdc_query_names_taxadb(sci_name = t.dat$db_taxon,
-                         db = database,
-                         rank_name = "Animalia",
-                         rank = "kingdom"
-  )
+# 
 
-# write some code to remove the output file
-unlink("Output", recursive=TRUE)
-
-harm.tax <- 
-  harm.tax %>%
-  mutate(db_taxon_higher_rank = ifelse(is.na(order) & is.na(family), NA, 
-                                       ifelse(is.na(order) & !is.na(family), "family", "order") ) ) %>%
-  mutate(db_taxon_higher = ifelse(is.na(order) & is.na(family), NA, 
-                                  ifelse(is.na(order), family, order) ) ) %>%
-  mutate(db_higher_rank_source = database) %>%
-  mutate(row_id = 1:n()) %>%
-  select(row_id, original_search, scientificName, acceptedNameUsageID, db_higher_rank_source, db_taxon_higher_rank, db_taxon_higher)
-
-# remove the names that we were not able to resolve
-harm.tax <- 
-  harm.tax %>%
-  filter(!(is.na(scientificName) |is.na(db_taxon_higher_rank) | is.na(db_taxon_higher) ) ) %>%
-  rename(db_taxon = original_search)
-
-# join these data to the tax.dat data
-t.clean <- right_join(t.dat, harm.tax, by = c("row_id", "db_taxon") )
-
-# check that the join worked correctly
-if ( nrow(harm.tax) == nrow(t.clean) ) {
-  message("Join worked correctly")
-}
-
-# remove the row_id column
-t.clean <- 
-  t.clean %>%
-  select(-row_id)
-
-gbif_htm
-
-v.x <- V(d.g) 
-v.x[which(attr(v.x, "names") == "Silvanidae")]
-
-distances(d.g, 
-          v.x[which(attr(v.x, "names") == "Silvanidae")],
-          v.x[which(attr(v.x, "names") == "Anexantha")],
-          mode = c("all"),
-          algorithm = c("bellman-ford"))
 
