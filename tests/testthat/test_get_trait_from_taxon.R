0a25e405365243f37be9c1424f4b87f3464389a0
Ronald Bergmann <info@black-snow.net>
2023-03-17 00:42:01 +0100

    fix formatting

--- tests/testthat/test_get_trait_from_taxon.R
+++ tests/testthat/test_get_trait_from_taxon.R
@@ -1,213 +1,206 @@
 make_test_input <- function() {
-    df_test1 <-
-        data.frame(
-            taxon_name = c(
-                "Gammarus", "Gammarus", "Gammarus", "Daphnia",
-                "Triops granitica", "Triops",
-                "Simocephalus vetulus", "Simocephalus vetulus",
-                "Turbellaria", "Oligochaeta", "Oligochaeta", "Daphnia gessneri"
-            ),
-            Life_stage = c(
-                "adult", "adult", "adult", "adult", "adult", "adult",
-                "adult", "adult", "none", "none", "none",
-                "adult"
-            ),
-            lat = c(rep(50.5, 6), rep(47.5, 6)),
-            lon = c(rep(4.98, 6), rep(-105.4, 6)),
-            body_size_mm = rnorm(12, 10, 2)
-        )
+  df_test1 <-
+    data.frame(
+      taxon_name = c(
+        "Gammarus", "Gammarus", "Gammarus", "Daphnia",
+        "Triops granitica", "Triops",
+        "Simocephalus vetulus", "Simocephalus vetulus",
+        "Turbellaria", "Oligochaeta", "Oligochaeta", "Daphnia gessneri"
+      ),
+      Life_stage = c(
+        "adult", "adult", "adult", "adult", "adult", "adult",
+        "adult", "adult", "none", "none", "none",
+        "adult"
+      ),
+      lat = c(rep(50.5, 6), rep(47.5, 6)),
+      lon = c(rep(4.98, 6), rep(-105.4, 6)),
+      body_size_mm = rnorm(12, 10, 2)
+    )
 
-    # add an NA to see how the functions react
-    df_test1[9, ]$body_size_mm <- NA
+  # add an NA to see how the functions react
+  df_test1[9, ]$body_size_mm <- NA
 
-    df_test1
+  df_test1
 }
 
 test_that("given a bad workflow,
             when get_trait_from_taxon,
             then error", {
-    expect_error(
-        get_trait_from_taxon(
-            data.frame(),
-            "tax", "larva", "lat", "lon", "size", "unsupported"
-        ),
-        ".*workflow.*"
-    )
+  expect_error(
+    get_trait_from_taxon(
+      data.frame(),
+      "tax", "larva", "lat", "lon", "size", "unsupported"
+    ),
+    ".*workflow.*"
+  )
 })
 
 test_that("if all taxon_names are not present in output,
            when get_trait_from_taxon,
            then error", {
-             input <- make_test_input()
-             
-             # when
-             output <- get_trait_from_taxon(
-               data = input,
-               target_taxon = "taxon_name",
-               life_stage = "Life_stage",
-               latitude_dd = "lat",
-               longitude_dd = "lon",
-               body_size = "body_size_mm",
-               max_tax_dist = 3,
-               trait = "equation",
-               gen_sp_dist = 0.5
-             )
-             
-             # test if all the relevant taxon names are present in the output
-             expect_true(all(output[["taxon_name"]] == input[["taxon_name"]]))
-             
-           })
+  input <- make_test_input()
+
+  # when
+  output <- get_trait_from_taxon(
+    data = input,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
+
+  # test if all the relevant taxon names are present in the output
+  expect_true(all(output[["taxon_name"]] == input[["taxon_name"]]))
+})
 
 test_that("if any individual body size is not within the taxon's minimum and maximum body size,
            when get_trait_from_taxon,
            then error", {
-             input <- make_test_input()
-             
-             # when
-             output <- get_trait_from_taxon(
-               data = input,
-               target_taxon = "taxon_name",
-               life_stage = "Life_stage",
-               latitude_dd = "lat",
-               longitude_dd = "lon",
-               body_size = "body_size_mm",
-               max_tax_dist = 3,
-               trait = "equation",
-               gen_sp_dist = 0.5
-             )
-             
-             # test if individual body sizes are within the min and max body size values
-             x <- (output[["body_size_mm"]] >= output[["min_body_size_mm"]]) & (output[["body_size_mm"]] <= output[["max_body_size_mm"]])
-             expect_true(all(x | is.na(x)))
-             
-           })
+  input <- make_test_input()
+
+  # when
+  output <- get_trait_from_taxon(
+    data = input,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
+
+  # test if individual body sizes are within the min and max body size values
+  x <- (output[["body_size_mm"]] >= output[["min_body_size_mm"]]) & (output[["body_size_mm"]] <= output[["max_body_size_mm"]])
+  expect_true(all(x | is.na(x)))
+})
 
 test_that("if the equation, body_size_mm or id columns are NA then dry_biomass_mg should be NA,
            when get_trait_from_taxon,
            then error", {
-             input <- make_test_input()
-             
-             # when
-             output <- get_trait_from_taxon(
-               data = input,
-               target_taxon = "taxon_name",
-               life_stage = "Life_stage",
-               latitude_dd = "lat",
-               longitude_dd = "lon",
-               body_size = "body_size_mm",
-               max_tax_dist = 3,
-               trait = "equation",
-               gen_sp_dist = 0.5
-             )
-             
-             # if the equation and body_size_mm or id colums are NA, then dry_biomass_mg should be NA
-             
-             # equation column
-             expect_true(all(is.na(output[["equation"]]) == is.na(output[["dry_biomass_mg"]])))
-             
-             # body_size_mm
-             x <- is.na(output[["body_size_mm"]])
-             expect_true(all(is.na(output[["body_size_mm"]][x]) == is.na(output[["dry_biomass_mg"]][x])))
-             
-             # id
-             y <- is.na(output[["id"]])
-             expect_true(all(is.na(c(output[["dry_biomass_mg"]][y]))))
-             
-           })
+  input <- make_test_input()
+
+  # when
+  output <- get_trait_from_taxon(
+    data = input,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
+
+  # if the equation and body_size_mm or id colums are NA, then dry_biomass_mg should be NA
+
+  # equation column
+  expect_true(all(is.na(output[["equation"]]) == is.na(output[["dry_biomass_mg"]])))
+
+  # body_size_mm
+  x <- is.na(output[["body_size_mm"]])
+  expect_true(all(is.na(output[["body_size_mm"]][x]) == is.na(output[["dry_biomass_mg"]][x])))
+
+  # id
+  y <- is.na(output[["id"]])
+  expect_true(all(is.na(c(output[["dry_biomass_mg"]][y]))))
+})
 
 test_that("if any of the outputted taxonomic distance values are greater than the max_tax_distance argument,
            when get_trait_from_taxon,
            then error", {
-             input <- make_test_input()
-             
-             # when
-             output <- get_trait_from_taxon(
-               data = input,
-               target_taxon = "taxon_name",
-               life_stage = "Life_stage",
-               latitude_dd = "lat",
-               longitude_dd = "lon",
-               body_size = "body_size_mm",
-               max_tax_dist = 3,
-               trait = "equation",
-               gen_sp_dist = 0.5
-             )
-             
-             # test if the outputted taxonomic distances are less than the max tax distance
-             expect_true(all(output[["tax_distance"]][!is.na(output[["tax_distance"]])] <= 3))
-             
-           })
+  input <- make_test_input()
 
-test_that("does the get_trait_from_taxon() function output the correct 
+  # when
+  output <- get_trait_from_taxon(
+    data = input,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
+
+  # test if the outputted taxonomic distances are less than the max tax distance
+  expect_true(all(output[["tax_distance"]][!is.na(output[["tax_distance"]])] <= 3))
+})
+
+test_that("does the get_trait_from_taxon() function output the correct
           additional identifier columns?", {
-            input <- make_test_input()
-            
-            # add additional columns
-            input$sex <- "male"
-            
-            # when
-            output <- get_trait_from_taxon(
-              data = input,
-              target_taxon = "taxon_name",
-              life_stage = "Life_stage",
-              latitude_dd = "lat",
-              longitude_dd = "lon",
-              body_size = "body_size_mm",
-              max_tax_dist = 3,
-              trait = "equation",
-              gen_sp_dist = 0.5
-            )
-            
-            # test if the outputted taxonomic distances are less than the max tax distance
-            expect_true( "sex" %in% names(output) )
-            
-          })
+  input <- make_test_input()
 
+  # add additional columns
+  input$sex <- "male"
+
+  # when
+  output <- get_trait_from_taxon(
+    data = input,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
+
+  # test if the outputted taxonomic distances are less than the max tax distance
+  expect_true("sex" %in% names(output))
+})
 
 test_that("test a highly marginal case where
             there are no matches for the life-stages", {
-    x <- get_trait_from_taxon(
-        data = data.frame(
-            taxon_name = c("Gammarus", "Daphnia"),
-            Life_stage = c("larva", "none"),
-            lat = rep(50.5, 1),
-            lon = rep(4.98, 1),
-            body_size_mm = rnorm(2, 10, 2)
-        ),
-        target_taxon = "taxon_name",
-        life_stage = "Life_stage",
-        latitude_dd = "lat",
-        longitude_dd = "lon",
-        body_size = "body_size_mm",
-        max_tax_dist = 3,
-        trait = "equation",
-        gen_sp_dist = 0.5
-    )
+  x <- get_trait_from_taxon(
+    data = data.frame(
+      taxon_name = c("Gammarus", "Daphnia"),
+      Life_stage = c("larva", "none"),
+      lat = rep(50.5, 1),
+      lon = rep(4.98, 1),
+      body_size_mm = rnorm(2, 10, 2)
+    ),
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
 
-    expect_true(all(is.na(x[, c("id", "tax_distance", names(x)[grepl("_match", x = names(x))])])))
-    
+  expect_true(all(is.na(x[, c("id", "tax_distance", names(x)[grepl("_match", x = names(x))])])))
 })
 
 test_that("test the case where there are only special names", {
-    input_data <- data.frame(
-        taxon_name = c("Oligochaeta", "Oligochaeta", "Turbellaria"),
-        Life_stage = c("none", "none", "none"),
-        lat = rep(50.5, 1),
-        lon = rep(4.98, 1),
-        body_size_mm = rnorm(3, 10, 2)
-    )
+  input_data <- data.frame(
+    taxon_name = c("Oligochaeta", "Oligochaeta", "Turbellaria"),
+    Life_stage = c("none", "none", "none"),
+    lat = rep(50.5, 1),
+    lon = rep(4.98, 1),
+    body_size_mm = rnorm(3, 10, 2)
+  )
 
-    x <- get_trait_from_taxon(
-        data = input_data,
-        target_taxon = "taxon_name",
-        life_stage = "Life_stage",
-        latitude_dd = "lat",
-        longitude_dd = "lon",
-        body_size = "body_size_mm",
-        max_tax_dist = 3,
-        trait = "equation",
-        gen_sp_dist = 0.5
-    )
+  x <- get_trait_from_taxon(
+    data = input_data,
+    target_taxon = "taxon_name",
+    life_stage = "Life_stage",
+    latitude_dd = "lat",
+    longitude_dd = "lon",
+    body_size = "body_size_mm",
+    max_tax_dist = 3,
+    trait = "equation",
+    gen_sp_dist = 0.5
+  )
 
-    expect_equal(input_data[["taxon_name"]], x[["taxon_name"]])
+  expect_equal(input_data[["taxon_name"]], x[["taxon_name"]])
 })
