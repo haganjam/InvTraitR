ce42ddff2fa7451da2b2f4d57eaa130150f70007
James Hagan <james_hagan@outlook.com>
2023-08-10 16:14:48 +0200

    add a script to export the reference database

---
+++ companion_scripts/02_create_database/03_create_reference_database.R
@@ -0,0 +1,23 @@
+#' @title reference list
+#' @description create a data.frame of the equation references
+#' @details This script compiles the list of publications from which the
+#'   equations in the equation database were compiled
+#' @author James G. Hagan (james_hagan(at)outlook.com)
+#' 
+
+# load relevant libraries
+library(dplyr)
+library(readxl)
+
+# load the reference list
+ref_dat <- readxl::read_xlsx(path = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_4_FreshInvTraitR/data/allometry_database_ver4/reference_database.xlsx")
+head(ref_dat)
+
+# check the database
+View(ref_dat)
+
+# replace the NA characters with true NAs
+ref_dat[ref_dat == "NA"] <- NA
+
+# export the database as a .rds file
+saveRDS(ref_dat, "database/reference_database.rds")
